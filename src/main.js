73c8fa6086074f30eb4b1b757941dd21f263c3a6
artanton <kabletrade@gmail.com>
2024-11-28 21:38:05 +0200

    main.js on scroll listner added our projscc improve animations performance

--- src/main.js
+++ src/main.js
@@ -6,43 +6,15 @@ const ctaOverlay = document.querySelector('.cta-overlay');
 const ctaText = document.querySelector('.cta-text');
 const ctaButton = document.querySelector('.cta-button');
 
-const poolShapeObserverCallback = (entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      poolShapeSection.classList.add('visible');
-      observer.unobserve(entry.target);
-    }
-  });
-};
 
-const poolShapeObserverOptions = {
-  root: null,
-  threshold: 0.1,
-};
-
-const poolShapeObserver = new IntersectionObserver(
-  poolShapeObserverCallback,
-  poolShapeObserverOptions
-);
-
-const projectObserverCallback = (entries, observer)=>{
-  entries.forEach(entry=>{
-if(entry.isIntersecting){
+const handleScroll = () => {
+  poolShapeSection.classList.add('visible');
   projectSection.classList.add('visible');
-  observer.unobserve(entry.target);
-}
-  });
+ 
+  window.removeEventListener('scroll', handleScroll);
 };
 
-const projectObserverOptions = {
-  root: null,
-  threshold:0.1
-}
-
-const projectObserver = new IntersectionObserver(
-  projectObserverCallback,
-  projectObserverOptions
-)
+window.addEventListener('scroll', handleScroll);
 
 
 
@@ -66,6 +38,5 @@ const ctaObserverOptions = {
 
 const ctaObserver = new IntersectionObserver(ctaObserverCallback, ctaObserverOptions);
 
-poolShapeObserver.observe(poolShapeSection);
-projectObserver.observe(projectSection);
+
 ctaObserver.observe(ctaSection);
