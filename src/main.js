883fbde524fcba1e536332ebc8ad864b1b3fda6b
andrii <a.hir@intobi.com>
2024-11-28 15:19:17 +0200

    pool-shapes observer

--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
+const poolShapeSection = document.querySelector('.pool-shape-section');
 const ctaSection = document.querySelector('.cta-section');
 const ctaContainer = document.querySelector('.cta-container');
 const ctaOverlay = document.querySelector('.cta-overlay');
 const ctaText = document.querySelector('.cta-text');
 const ctaButton = document.querySelector('.cta-button');
 
-const observerCallback = (entries, observer) => {
+const poolShapeObserverCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      poolShapeSection.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+const poolShapeObserverOptions = {
+  root: null,
+  threshold: 0.1,
+};
+
+const poolShapeObserver = new IntersectionObserver(
+  poolShapeObserverCallback,
+  poolShapeObserverOptions
+);
+
+
+
+const ctaObserverCallback = (entries, observer) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       ctaContainer.classList.add('animate-cta-container');
@@ -17,11 +39,12 @@ const observerCallback = (entries, observer) => {
   });
 };
 
-const observerOptions = {
+const ctaObserverOptions = {
   root: null,
   threshold: 0.8,
 };
 
-const observer = new IntersectionObserver(observerCallback, observerOptions);
+const ctaObserver = new IntersectionObserver(ctaObserverCallback, ctaObserverOptions);
 
-observer.observe(ctaSection);
+poolShapeObserver.observe(poolShapeSection);
+ctaObserver.observe(ctaSection);
